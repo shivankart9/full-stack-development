d4fb6dcac14c62f22a1e149f9bbd1a942d485a08
shivankart9 <shivankart9@gmail.com>
2026-08-30 07:28:15 +0530

    studied about operations and some tricky operations which are not prefered

---
+++ learning-javascript/01_basics/04_operations.js
@@ -0,0 +1,44 @@
+let value = 3
+let negvalue = -value
+
+console.log(negvalue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3) //power
+console.log(2/3)
+console.log(2%3)
+
+let str1 ="hello"
+let str2 ="shivankar"
+let str3 = str1 + str2
+
+console.log(str3)
+
+// problem
+
+console.log("1" + 2) // 12
+console.log(1 + "2") // 12
+console.log("1" + 2 + 2)  // 122
+console.log(1 + 2 + "2") // 32
+
+// tricky operations not prefered this create confusion
+
+console.log(true) // true
+console.log(+true) // 1
+
+console.log(+"") // 0
+
+// not prefered this create confusion
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2+2
+
+// ++
+
+let gameCounter = 100
+gameCounter++;
+
+console.log(gameCounter)
