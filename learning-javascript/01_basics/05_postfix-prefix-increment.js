7b62311ef5f61b13801c54387e124ac9368cd3d9
shivankart9 <shivankart9@gmail.com>
2026-08-30 21:55:58 +0530

    understood prefix and postfix increment

---
+++ learning-javascript/01_basics/05_postfix-prefix-increment.js
@@ -0,0 +1,14 @@
+// In postfix increment (a++) it returns the value before increment and  in prefix increment(++a) it returns the value after increment
+
+// postfix
+let x = 5
+let y = x++
+
+console.log(y)
+
+// prefix
+let a = 5
+let b = ++a
+
+console.log(b)
+
