9ab7723682fad0a5a1e754c6b041b3d43d34ac7f
shivankart9 <shivankart9@gmail.com>
2026-09-01 04:56:31 +0530

    comparision of data types

---
+++ learning-javascript/01_basics/06_comparision-data-types.js
@@ -0,0 +1,23 @@
+// console.log(2>1)
+// console.log(2>=1)
+// conso;e.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// when we do comparision we should always remember that we should only compare same type of data type if we do not do so sometimes we could not get the expected output
+
+console.log("2" > 1) // true
+console.log("02" > 1) // true
+
+// output are not predictable
+
+console.log(null > 0) // false
+console.log(null == 0) // false
+console.log(null >= 0) // true
+
+// strict check ===
+console.log("2" === 2) // false
+
+// avoid other comparision use strict check
+
+
