c2e8754403f5812888b190d54a6402415558e500
shivankart9 <shivankart9@gmail.com>
2026-09-03 20:46:36 +0530

    studied about the output that we get when we use type of

---
+++ learning-javascript/01_basics/08_data-type-results.js
@@ -0,0 +1,12 @@
+// data type results when we  will do type of
+
+// value = undefined , result = undefined
+// value = null , result = object
+// value = true/false(boolean) , result = boolean
+// value = 1,3.14,NaN(number) , result = number
+// value = "hello"(string) , result =  string
+// value = 123n(bigint) , result = bigint
+// value = symbol , result = symbol
+// value = {name : "john"}(object) , result = object
+// value = [1,2,3](array) , result = object
+// value = function() {} , result = function
