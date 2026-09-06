3e2343b4c2f778fd97d8988729cc2c07adeb827f
shivankart9 <shivankart9@gmail.com>
2026-09-06 07:06:38 +0530

    studied string manipulation and recommened syntax as well i made a new folder where i will add ss of the js code that i will do in browser console

---
+++ learning-javascript/01_basics/10_string.js
@@ -0,0 +1,14 @@
+const name = "shivankar"
+
+const repoCount = 15
+
+console.log(name + repoCount + " Shivankart9") // shivankar15 Shivankart9 // Not recommended syntax for concatination
+
+
+// recommended syntax modern way it is called as string manipulation
+
+console.log(`My name is ${name} I have made ${repoCount} repositries in my github account`)
+
+// another method to declare string
+
+const gamename = new String('shivankar')
