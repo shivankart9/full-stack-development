81077b2378782850930db34d632e182524544fa0
shivankart9 <shivankart9@gmail.com>
2026-09-14 06:32:22 +0530

    studied about nums funstions and 2 math function 1- abs() 2- round() as well added to ss of browser console in which there was math library and number functions

--- learning-javascript/01_basics/11_nums-and-maths.js
+++ learning-javascript/01_basics/11_nums-and-maths.js
@@ -16,3 +16,17 @@ console.log(score.toFixed(3)) //100.000
 const othernumber = 82.3685
 
 console.log(othernumber.toPrecision(4)) //82.37 //returns value in string and do approximation upto where we assign value . value can be assigned only between 1-21
+
+const hundreads = 1000000
+
+console.log(hundreads.toLocaleString()) //1,000,000 // make easy to read zeroes
+
+console.log(hundreads.toLocaleString('en-IN')) //according to india standard
+
+// +++++++++++++++++++++++++++ MATHS +++++++++++++++++++++++++++++++++
+
+console.log(Math)
+
+console.log(Math.abs(-7)) //abs convert to positve value from negative //7
+
+console.log(Math.round(9.9)) // removes negative value and do round of // 10
