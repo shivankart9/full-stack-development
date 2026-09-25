eef5d39181a8c5d62312b888b59a2192a4464637
shivankart9 <shivankart9@gmail.com>
2026-09-25 21:42:58 +0530

    studied object handling and array handling in function

--- learning-javascript/03_basics/01_functions.js
+++ learning-javascript/03_basics/01_functions.js
@@ -35,9 +35,45 @@ function loginUserMsg(username = "ramdev") { // ramdev here  is the parameter so
     return `${username} just logged in`
 }
 
-// console.log(loginUserMsg("shivankar")) //shivankr just logged in
+// console.log(loginUserMsg("shivankar")) //shivankar just logged in
 
 console.log(loginUserMsg()) // ramdev just logged in
 
+// functions with objects and array
 
+function calculateCartPrice(val1,val2,...num1) { //... this dot dot is known as rest operator as well spread operator but it depends upon its usecase what we call it
+    return num1;
+}
 
+console.log(calculateCartPrice(200,400,500,2000)) // it will print a array which will include 500 and 2000 only as starting two values are stored in val1 and val2
+
+const user = {
+    price: 99,
+    username: "shivankar"
+}
+
+function handleObject(anyobject) {
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+// handleObject(user)
+
+// direct 
+handleObject({
+    username: "shivankar",
+    price:99
+})
+
+// array handling
+
+const myNewArray = [100,2000,5000,400]
+
+function handleArray(anyArray) {
+    return anyArray[1]
+}
+
+// console.log(handleArray(myNewArray))
+
+// direct 
+
+console.log(handleArray([100,2000,5000,400])) // 200
